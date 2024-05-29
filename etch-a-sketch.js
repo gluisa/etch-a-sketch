8ceabc65947c10317e6e67a028c5b9dfcfaa17d1
gluisa <76462853+gluisa@users.noreply.github.com>
2024-05-29 11:35:57 -0400

    Created css, js files and linked them to the html

---
+++ etch-a-sketch.js
@@ -0,0 +1,19 @@
+const container = document.querySelector("#container");
+
+function makeGrid() {
+    for (i =0; i < 16; i++){
+        const row = document.createElement("div");
+        row.className = "row";
+        container.appendChild(row);
+        row.textContent = i;
+
+        for (j=0; j < 16; j++) {
+            const col = document.createElement("div");
+            col.className = "col";
+            row.appendChild(col);
+            col.textContent = j;
+        }
+    }
+}
+
+makeGrid();
