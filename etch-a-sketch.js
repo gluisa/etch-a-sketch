bc32d098df2d1af13eb05a998c87afac8a028ba4
gluisa <76462853+gluisa@users.noreply.github.com>
2024-06-19 09:54:49 -0400

    Completeing grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,25 +1,19 @@
-function makeGrid() {
-    const container = document.querySelector("#container");
-    const box = document.createElement("div");
-    box.className = "box";
-    container.appendChild(box);
-}
+const container = document.querySelector(".container");
 
-    /*
-    for (i =0; i < 16; i++){
-        const row = document.createElement("div");
-        row.className = "row";
-        container.appendChild(row);
-        row.textContent = i;
+  function makeGrid() {
+    for (i = 0; i < 16; i++) {
+      const row = document.createElement("div");
+      row.className = "row";
+      container.appendChild(row);
+      row.textContent = i;
 
-        for (j=0; j < 16; j++) {
-            const col = document.createElement("div");
-            col.className = "col";
-            row.appendChild(col);
-            col.textContent = j;
-        }
+      for (j = 0; j < 16; j++) {
+        const col = document.createElement("div");
+        col.className = "col";
+        row.appendChild(col);
+        col.textContent = j;
+      }
     }
-}
-*/
+  }
 
-makeGrid();
+  makeGrid();
